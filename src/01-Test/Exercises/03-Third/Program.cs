755a28937f8b37c0ef4825df2293824fc0393f4c
Mihkel Otto <mihkel.otto@tptlive.ee>
2017-11-13 02:28:33 +0200

    test2

--- src/01-Test/Exercises/03-Third/Program.cs
+++ src/01-Test/Exercises/03-Third/Program.cs
@@ -10,7 +10,7 @@ namespace _03_Third
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Third");
+            Console.WriteLine("Third test");
         }
     }
 }
