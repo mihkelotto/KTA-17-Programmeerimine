faef82b656ffd2630547aa6f198271b9dc17f9f9
mihkelotto <32923378+mihkelotto@users.noreply.github.com>
2017-11-17 13:56:32 +0200

    Create new solution

---
+++ src/03-graded-assignment/GradedAssignments-17.11/01-Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Sisesta number: ");
+
+            string input = Console.ReadLine();
+
+            int userNumber = int.Parse(input);
+
+            Console.WriteLine(userNumber);
+
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+        }
+    }
+}
