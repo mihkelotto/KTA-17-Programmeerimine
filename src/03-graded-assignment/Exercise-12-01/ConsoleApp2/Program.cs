6567d5b2b3c484092fcebd927a1dd7e5d268c77b
mihkelotto <32923378+mihkelotto@users.noreply.github.com>
2018-02-12 16:42:55 +0200

    Added

---
+++ src/03-graded-assignment/Exercise-12-01/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+
+            string array = new String[3];
+
+            rng = new Random();
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n);
+                n--;
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+
+            }
+    }
+}
