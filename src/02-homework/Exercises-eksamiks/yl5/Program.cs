e0363a2fec4811feed42c30ec8b7da258496b76d
Mihkel Otto <mihkel.otto@tptlive.ee>
2018-04-09 02:37:43 +0300

    Added code

---
+++ src/02-homework/Exercises-eksamiks/yl5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yl5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contents = "suvaline tekst";
+            System.IO.File.WriteAllText(@"C:\Users\mihke\Desktop\path.txt", contents);
+        }
+    }
+}
